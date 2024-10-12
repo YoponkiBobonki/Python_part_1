8d5b5ecebca8458e67644a367e3ccc998e7cfec4
Miron <Miran113099@gmail.com>
2024-10-12 21:13:34 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Задача 3. Квадраты нечётных чисел
+
+n = int(input("Введите число : "))
+for i in range(1, n // 2 + 2):
+    summ = i * 2 - 1
+    square = summ ** 2
+    print(summ, "** 2 = ", square)
